a536b396e54a2ca43ec9909aabdd604ff8168874
Mike Munhall <munhall.mike@gmail.com>
2016-03-24 09:07:03 -0600

    Initial commmit of code.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# cam-poster
-A brutish script for the Raspberry Pi that periodically fires a the camera and posts images via FTP.
+cam-poster
+==========
+
+Description
+-----------
+
+This project is a brutish script for the Raspberry Pi that periodically fires the camera and posts the resulting images via FTP to a remote server.
+
+This is the first part of a quick-and-dirty "webcam" project. The other half of this project (TBD) displays the resulting images on a webpage.
+
+There is a lot of manual setup, all of which is documented in this README.
+
+Directory Structure
+-------------------
+
+### /config ###
+
+Configurations can be stored here. The directory contains a sample configuration. Generally, a configuration would (and probably should) live outside the application.
+
+### /images ###
+
+Images captured by the camera can be stored here. Generally, images can be (and probably should), be stored outside the application.
+
+### /init.d ###
+
+Contains the init.d script used to start the app at boot.
+
+### /src ###
+
+Contains the Node.js application.
+
+Setup
+-----
---
+++ src/poster.js
@@ -0,0 +1,98 @@
+var JsFtp = require('jsftp'),
+	fs = require('fs'),
+	nconf = require('nconf'),
+	exec = require('child_process').exec,
+	counter = 0;
+
+configure();
+init();
+
+function configure() {
+	console.log("configure");
+	var file;
+	if (process.argv.length < 3) {
+	 	console.log("Configuration file must be specifed as the first argument.");
+		process.exit;
+	}
+	file = process.argv[2];
+	if (!fs.existsSync(file)) {
+		console.log("Configuration file does not exist.");
+		process.exit;	
+	}
+	nconf.use('memory');
+	nconf.file({ file: process.argv[2] });
+}
+
+function init() {
+	console.log('init');
+	makeDirectory();
+	capture();
+}
+
+function makeDirectory() {
+	console.log('makeDirectory');
+	if (!fs.existsSync(nconf.get("localImages:directory"))) {
+	    fs.mkdirSync("imgs");
+	}
+}
+
+function capture() {
+	console.log('capture: ' + counter);
+	exec("raspistill --width 800 --height 600 -o " + getImageLocalPath(), onCapture);
+}
+
+function onCapture(error, stdout, stderror) {
+	console.log('onCapture: ' + counter);
+	if (error !== null) {
+		console.log("error: " + error);
+		console.log("stderror: " + stderror);
+		// TODO: Recover (restart in 10 minutes?)
+	} else {
+		put();
+	}
+}
+
+function put() {
+	console.log('put: ' + counter);
+	var ftp = new JsFtp({
+	  host: nconf.get("ftp:host"),
+	  port: nconf.get("ftp:port"),
+	  user: nconf.get("ftp:username"),
+	  pass: nconf.get("ftp:password"),
+	  debugMode: nconf.get("ftp:debug")
+	});
+
+	ftp.on('jsftp_debug', function(eventType, data) {
+	  console.log('DEBUG: ', eventType);
+	  console.log(JSON.stringify(data, null, 2));
+	});
+	
+	ftp.put(getImageLocalPath(), nconfg.get("ftp:directory") + getImageName(), function(err) {
+		if (err) {
+			// TODO: Recover
+			console.log("put err: " + err);
+			ftp.raw.quit();
+		} else {
+			ftp.raw.quit();
+			setCounter();
+	    	setTimeout(capture, 60000);
+	    }
+	});
+}
+
+function setCounter() {
+	console.log('setCounter: ' + counter);
+	if (counter == 9) {
+		counter = 0;
+	} else {
+		counter = counter + 1;
+	}
+}
+
+function getImageName() {
+	return "img" + counter + ".jpg";
+}
+
+function getImageLocalPath() {
+	return "imgs/" + getImageName();
+}
